6f5be3a0fc226e3010bbf9a3672cd3fad3c3bdf3
JoJaCo <contrerasjorge226@gmail.com>
2020-11-08 00:01:56 -0800

    updated myself on findbyid still need to do some reaserch

--- app/src/main/java/com/calivera/andriod/ageinmin/MainActivity.kt
+++ app/src/main/java/com/calivera/andriod/ageinmin/MainActivity.kt
@@ -2,10 +2,38 @@ package com.calivera.andriod.ageinmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        val btnDataPicker: Button = findViewById(R.id.btnDataPicker)
+
+
+        btnDataPicker.setOnClickListener{
+            Toast.makeText(this,
+            "hello world",
+            Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
